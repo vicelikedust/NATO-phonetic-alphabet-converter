f7ca1eee7f9b5d71e13fdc4c4fb2cadb3ac8416e
vicelikedust <toolboxes909@gmail.com>
2022-02-08 13:25:17 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace NATO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.MaximumSize = new System.Drawing.Size(240, 415);
+            this.textBox1.MinimumSize = new System.Drawing.Size(240, 415);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 415);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 23);
+            this.textBox2.MinimumSize = new System.Drawing.Size(40, 415);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(515, 415);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "NATO phonetic alphabet";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NATO
+{
+    public partial class Form1 : Form
+    {
+        IDictionary<string, string> AlphabetList = new Dictionary<string, string>() {
+            {"a","alfa"},{"b","bravo"},{"c","charlie"},{"d","delta"},{"e","echo"},{"f","foxtrot"},{"g","golf"},{"h","hotel"},{"i","india"},
+            {"j","juliett"},{"k","kilo"},{"l","lima"},{"m","mike"},{"n","november"},{"o","oscar"},{"p","papa"},{"q","quebec"},{"r","romeo"},
+            {"s","sierra"},{"t","tango"},{"u","uniform"},{"v","victor"},{"w","whiskey"},{"x","x-ray"},{"y","yankee"},{"z","zulu"},
+            {"A","Alfa"},{"B","Bravo"},{"C","Charlie"},{"D","Delta"},{"E","Echo"},{"F","Foxtrot"},{"G","Golf"},{"H","Hotel"},{"I","India"},
+            {"J","Juliett"},{"K","Kilo"},{"L","Lima"},{"M","Mike"},{"N","November"},{"O","Oscar"},{"P","Papa"},{"Q","Quebec"},{"R","Romeo"},
+            {"S","Sierra"},{"T","Tango"},{"U","Uniform"},{"V","Victor"},{"W","Whiskey"},{"X","X-ray"},{"Y","Yankee"},{"Z","Zulu"},
+            {" "," "}
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            await Task.Run(() => UpdateUI());
+        }
+        private async void UpdateUI()
+        {
+            textBox2.Invoke(new MethodInvoker(async delegate {
+                textBox2.Text = await Task.Run(() => ConvertText());
+            }));
+        }
+        private string ConvertText()
+        {
+            string newString = "";
+            foreach (char c in textBox1.Text)
+            {
+                if (AlphabetList.ContainsKey(c.ToString()))
+                {
+                    newString += $"{AlphabetList[c.ToString()]} ";
+                }
+                else
+                {
+                    newString += c.ToString();
+                }
+            }
+            return newString;
+        }
+    }
+}
